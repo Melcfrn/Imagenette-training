1c595bacb02b336eb3070fa14cb861c5c2e984dc
Melcfrn <clement.91m@gmail.com>
2022-06-15 12:42:10 +0200

    ajout notebook

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ from pytorch_classifier import PytorchClassifier
 # Dataset + parameters                                                        #
 ###############################################################################
 
-batch_size = 128
-nb_epochs = 5
+batch_size = 64
+nb_epochs = 20
 transform = transforms.Compose(
     [transforms.ToTensor(),
-     transforms.Resize((160, 160)),
+     transforms.Resize((320, 320)),
      transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))]
      )
-root_to_train = "imagenette2-160/train"
-root_to_test = "imagenette2-160/val"
+root_to_train = "imagenette2-320/train"
+root_to_test = "imagenette2-320/val"
 
 trainloader = Loader(location=root_to_train, transform=transform,
                      batch_size=batch_size, shuffle=True).get_dataset()
@@ -29,7 +29,7 @@ testloader = Loader(location=root_to_test, transform=transform,
 
 net = Model()
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.8)
+optimizer = optim.Adam(net.parameters(), lr=0.01)
 
 ###############################################################################
 # Training                                                                    #
@@ -43,4 +43,4 @@ classifier = PytorchClassifier(model=net,
 
 classifier.fit(dataset=trainloader, nb_epochs=nb_epochs)
 classifier.predict(dataset=testloader)
-classifier.save(filename="first_model.pt")
+classifier.save(filename="first_model_320.pt")
--- model.py
+++ model.py
@@ -21,15 +21,25 @@ class Model(nn.Module):
             nn.Conv2d(3, 32, kernel_size=self.kernel_size, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(32),
+            nn.Dropout(0.3),
             nn.Conv2d(32, 64, kernel_size=self.kernel_size, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.3),
             nn.Conv2d(64, 128, kernel_size=self.kernel_size, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(128),
+            nn.Dropout(0.3),
 
             nn.Flatten(),
-            nn.Linear(128 * 18 * 18, 10)
+            nn.Linear(128 * 38 * 38, 100),
+            nn.ReLU(),
+            nn.BatchNorm1d(100),
+            nn.Dropout(0.3),
+            nn.Linear(100, 10)
         )
 
     def forward(self, x):
